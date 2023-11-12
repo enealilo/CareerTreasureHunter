07294340ff942324dfe83e64220f88e83a9ab35d
User <enealilo1996@gmail.com>
2023-11-12 09:29:50 +0100

    Fixing Lint Problems

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,5 +1,5 @@
 import "./LoginSignup.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
